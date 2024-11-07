8cffbc3a8b030d08abdf8d64149755095743e115
GitShailendra <kumarshailrndra7355@gmail.com>
2024-11-07 16:33:15 +0530

    fix:development url

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -18,3 +18,26 @@ exports.registerStudent = async (req, res) => {
     res.status(500).json({ error: 'An error occurred while registering the student' });
   }
 };
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user in the database
+    const user = await Student.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    // Compare the plain-text password
+    if (password !== user.password) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    // Generate a token
+    
+
+    res.json({ message: "Login successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- routes/Student.js
+++ routes/Student.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerStudent } = require('../controllers/studentController');
+const { registerStudent ,login} = require('../controllers/studentController');
 
 // Route to register a student
 router.post('/', registerStudent);
-
+router.post("/login",login)
 module.exports = router;
